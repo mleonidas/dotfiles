f59e937d290636f07457b5e654843e07cdf5473d
Mike Leone <mleone896@gmail.com>
2022-08-06 11:16:21 -0400

    update language server config

--- nvim/.config/nvim/lua/mleonidas/lsp.lua
+++ nvim/.config/nvim/lua/mleonidas/lsp.lua
@@ -189,11 +189,4 @@ end
 
 require("luasnip.loaders.from_vscode").lazy_load()
 
-local tabnine = require('cmp_tabnine.config')
-tabnine:setup({
-	max_lines = 1000,
-	max_num_results = 20,
-	sort = true,
-	run_on_every_keystroke = true,
-	snippet_placeholder = "..",
-})
+
