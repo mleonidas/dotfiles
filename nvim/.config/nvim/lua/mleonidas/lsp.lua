fd370b9bfca3906a003c0074ce40e188ec6d8c4e
Mike Leone <mleone896@gmail.com>
2022-08-06 11:16:03 -0400

    update completion engine

--- nvim/.config/nvim/lua/mleonidas/lsp.lua
+++ nvim/.config/nvim/lua/mleonidas/lsp.lua
@@ -190,17 +190,10 @@ end
 require("luasnip.loaders.from_vscode").lazy_load()
 
 local tabnine = require('cmp_tabnine.config')
-
-tabnine.setup({
+tabnine:setup({
 	max_lines = 1000,
 	max_num_results = 20,
 	sort = true,
 	run_on_every_keystroke = true,
-	snippet_placeholder = '..',
-	ignored_file_types = { 
-		-- default is not to ignore
-		-- uncomment to ignore in lua:
-		-- lua = true
-	},
-	show_prediction_strength = false
+	snippet_placeholder = "..",
 })
