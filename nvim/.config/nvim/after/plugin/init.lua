1c431c9660aebb597668007bd8e318f7ffa34056
Mike Leone <mleone896@gmail.com>
2023-02-13 17:03:06 -0500

    wip

--- nvim/.config/nvim/after/plugin/init.lua
+++ nvim/.config/nvim/after/plugin/init.lua
@@ -1,4 +1,18 @@
 -- vim.cmd(":TSInstall all");
 --
-require('Comment').setup()
+require("Comment").setup()
 
+local rt = require("rust-tools")
+
+rt.setup({
+    server = {
+        on_attach = function(_, bufnr)
+            -- Hover actions
+            vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+            -- Code action groups
+            vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+        end,
+    },
+})
+
+require('rust-tools').inlay_hints.enable()
