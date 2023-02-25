dc658aa37f46231aedb0004fe28a90bdd7780b98
Mike Leone <mleone896@gmail.com>
2023-02-25 16:33:23 -0500

    {WIP}

--- nvim/.config/nvim/queries/authzed/highlights.scm
+++ nvim/.config/nvim/queries/authzed/highlights.scm
@@ -1,23 +1,15 @@
-((relation_literal) @keyword)
+; highlights.scm
+((identifier) @keyword
+ (#match? @keyword "^(definition|permission|relation)$"))
 
+((permission_literal) @function)
+((relation_literal) @function)
 
-((perm_expression) (identifier) @operator)
-
-(block (identifier) @function)
-(block (identifier) (identifier) @type)
-
-((permission (permission_literal) @variable.builtin))
-
-((perm_expression) @operator)
-
-[
-  (rel_expression)
-  (perm_expression)
-] @function
-
-[
- (plus_literal)
- (pipe_literal)
-] @punctuation
-
-(comment) @comment
+(permission (identifier) @type)
+(relation (identifier) @constant)
+(perm_expression (identifier) @property)
+(rel_expression (identifier) @property)
+((block_start) @punctuation)
+((block_end) @punctuation)
+(block (identifier) (identifier) @constructor)
+((plus_literal) @punctuation)
