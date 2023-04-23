c02477285d2cb63303fd86b5bf9795f099110cef
Mike Leone <mleone896@gmail.com>
2023-04-23 14:43:23 -0400

    feat: 🎸 add term lib

---
+++ nvim/.config/nvim/lua/mleonidas/term.lua
@@ -0,0 +1,40 @@
+local M = {}
+
+local Terminal = require("toggleterm.terminal").Terminal
+
+-- Git client
+local git_tui = "lazygit"
+-- local git_tui = "gitui"
+
+-- Committizen
+local git_cz = "git cz"
+
+local git_client = Terminal:new({
+	cmd = git_tui,
+	dir = "git_dir",
+	hidden = true,
+	direction = "float",
+	float_opts = {
+		border = "double",
+	},
+})
+
+local git_commit = Terminal:new({
+	cmd = git_cz,
+	dir = "git_dir",
+	hidden = true,
+	direction = "float",
+	float_opts = {
+		border = "double",
+	},
+})
+
+function M.git_client_toggle()
+	git_client:toggle()
+end
+
+function M.git_commit_toggle()
+	git_commit:toggle()
+end
+
+return M
