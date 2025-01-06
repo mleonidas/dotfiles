8e7e8ecca3e33d011ef9179f248c760c19de47bd
Michael Leone <mleone896@gmail.com>
2025-01-05 21:16:42 -0500

    feat: add back go pluging

--- nvim/.config/nvim/lua/mleonidas/lazy.lua
+++ nvim/.config/nvim/lua/mleonidas/lazy.lua
@@ -239,4 +239,18 @@ return require("lazy").setup({
 			require("neoclip").setup()
 		end,
 	},
+	{
+		"ray-x/go.nvim",
+		dependencies = { -- optional packages
+			"ray-x/guihua.lua",
+			"neovim/nvim-lspconfig",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		config = function()
+			require("go").setup()
+		end,
+		event = { "CmdlineEnter" },
+		ft = { "go", "gomod" },
+		build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+	},
 })
