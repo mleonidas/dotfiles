bbd7105159deb759afd0df6454ceec4df03546aa
Michael Leone <mleone896@gmail.com>
2025-01-20 08:13:04 -0500

    {WIP}

--- nvim/.config/nvim/lua/mleonidas/plugins/smartplits.lua
+++ nvim/.config/nvim/lua/mleonidas/plugins/smartplits.lua
@@ -15,21 +15,4 @@ return {
 			"rcarriga/nvim-notify",
 		},
 	},
-	-- there's got to be a better way
-	"vim-test/vim-test",
-	"godlygeek/tabular",
-	"numToStr/Comment.nvim",
-	"kyazdani42/nvim-web-devicons",
-	"simrat39/rust-tools.nvim",
-	"simrat39/symbols-outline.nvim",
-	"lewis6991/gitsigns.nvim",
-	"tpope/vim-fugitive",
-	"vim-crystal/vim-crystal",
-	-- "github/copilot.vim",
-	{
-		"supermaven-inc/supermaven-nvim",
-		config = function()
-			require("supermaven-nvim").setup({})
-		end,
-	},
 }
